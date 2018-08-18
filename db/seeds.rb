5540f93883cae3917c2d29fe6e37468119d42757
AngelaDiSalvo <angela.disalvo@gmail.com>
2018-08-18 17:29:22 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.create(name: "dogs")
+Category.create(name: "cats")
+Category.create(name: "birds")
+
+
+Post.create(title: "my dog like cheezits", content: "he eats them everyday")
+Post.create(title: "my cat is mad", content: "she almosr caught a mouse but then it got away")
+Post.create(title: "my bird is funny", content:"he eats spaghetti and also feeds it to my dog")
+
+
+PostCategory.create(category_id: 1, post_id: 1)
+PostCategory.create(category_id: 2, post_id: 2)
+PostCategory.create(category_id: 3, post_id: 3)
